51da614165c25fb636c642c20528e721db92abc6
Julianmenav <julianmenav2@gmail.com>
2022-05-13 19:27:53 +0200

    todo app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-
+import { useState } from "react"
 
 const App = () => {
-  return <h1>Helloworld</h1>
+  const [input, setInput] = useState("")
+  const [toDolist, setToDoList] = useState([])
+
+  const handleChange = (e) => {
+    if(e.target.value.endsWith('\n')) return handleSubmit()
+    setInput(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    if (input === "") return
+    setToDoList([...toDolist, input])
+    console.log(input)
+    setInput("")
+  }
+
+  const handleClean = () => {
+    setToDoList([])
+  }
+
+  return (
+    <div className='container'>
+      <h1> To do List </h1>
+      <textarea
+        placeholder="Add an item."
+        onChange={handleChange}
+        value={input}
+      />
+      <br />
+      <div>
+        <button onClick={handleSubmit}>ADD</button>
+        <button onClick={handleClean}>CLEAN</button>
+      </div>
+      <ul>
+        {toDolist.map((element, key) => <li key={key}>{element}</li>)}
+      </ul>
+    </div>
+  )
 }
 
 
